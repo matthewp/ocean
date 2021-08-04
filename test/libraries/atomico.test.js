f15e62a73ca858acb9437dcbf25ed1d4a2d800fa
Matthew Phillips <matthew@phillipsoft.com>
2021-08-04 06:24:52 -0400

    Get atomico working

--- test/libraries/atomico.test.js
+++ test/libraries/atomico.test.js
@@ -1,12 +1,12 @@
 import '../../lib/shim.js?global';
-//import { c, html as ahtml } from 'https://cdn.skypack.dev/atomico@1.27.0';
+import { c, html as ahtml } from 'https://cdn.skypack.dev/atomico@1.27.0';
 import { Ocean } from '../../lib/mod.js';
-import { consume, document, customElements } from '../helpers.js';
-import { assertStringIncludes } from '../deps.js';
+import { consume, document, customElements, parse } from '../helpers.js';
+import { assert, assertEquals } from '../deps.js';
 
 Deno.test({
   name: 'Atomico elements render',
-  ignore: true,
+  ignore: false,
   fn: async () => {
     function component() {
       return ahtml`
@@ -22,5 +22,11 @@ Deno.test({
     let iter = html`<atomico-el></atomico-el>`;
     let out = await consume(iter);
     console.log(out);
+    let doc = parse(out);
+    let cel = doc.querySelector('atomico-el');
+    assert(cel);
+
+    assert(cel.querySelector('template h1'));
+    assertEquals(cel.querySelector('template h1').textContent, 'Testing');
   }
 });
